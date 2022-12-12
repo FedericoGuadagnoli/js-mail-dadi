b49f8875e7cfddc357690144f4d82cdbf72be7b2
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2022-12-12 13:20:37 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log('JS OK');
+
+// ESERCIZIO 1 - Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Stampiamo in pagina i due tiri e il risultato
+// -----------------------------------------------------------------
+
+//Prendo gli elementi dalla pagina
+const button = document.getElementById('button');
+const userNumber = document.getElementById('user-number');
+const cpuNumber = document.getElementById('cpu-number');
